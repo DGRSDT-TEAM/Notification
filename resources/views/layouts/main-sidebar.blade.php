7e5cf304d9e81d326602ed5f138bdbdc8d0fd64e
Younes-DZ <younes.abderrahmane.dgrsdt@gmail.com>
2021-11-22 00:09:03 -1100

    install theme

---
+++ resources/views/layouts/main-sidebar.blade.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+    <div class="row">
+        <!-- Left Sidebar start-->
+        <div class="side-menu-fixed">
+            <div class="scrollbar side-menu-bg">
+                <ul class="nav navbar-nav side-menu" id="sidebarnav">
+                    <!-- menu item Dashboard-->
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#dashboard">
+                            <div class="pull-left"><i class="ti-home"></i><span class="right-nav-text">Dashboard</span>
+                            </div>
+
+                            <div class="clearfix"></div>
+                        </a>
+
+                    </li>
+
+                    <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title">Ajouter </li>
+
+
+
+                    <li>
+                        <a href="chat-page.html"><i class="ti-comments"></i><span class="right-nav-text">Notification
+                            </span></a>
+                    </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Left Sidebar End-->
+
+        <!--=================================
